db384671d7dc6a69525415a85661fffbc09446b8
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2015-05-15 01:22:47 +0530

    added post advertisement page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
 
-// $(document).ready(function(){
-    var highlightLink = function(){
-        console.log('inside');
-    var path = window.location.pathname;
+var highlightLink = function(){
+    console.log('inside');
+var path = window.location.pathname;
 
-    $('.header .align-middle p').removeClass('active-url');
+$('.header .align-middle p').removeClass('active-url');
 
-    // if('/^\/about-us.html/'.test(path)){
-    // if(/^\/samprathi/.test(path)){
-        if (path.indexOf('index.html') > 0)
-            $('[data-url=about]').addClass('active-url');
-        else if(path.indexOf('post-advertisement.html') > 0)
-            $('[data-url=adds]').addClass('active-url');
-        else if(path.indexOf('recommended.html') > 0)
-            $('[data-url=recommended]').addClass('active-url');
-    };
+    if (path.indexOf('index.html') > 0)
+        $('[data-url=about]').addClass('active-url');
+    else if(path.indexOf('post-advertisement.html') > 0)
+        $('[data-url=adds]').addClass('active-url');
+    else if(path.indexOf('recommended.html') > 0)
+        $('[data-url=recommended]').addClass('active-url');
+};
 
 $(document).ready(function(){
     highlightLink();
 })    
-    
-// })

1c206afe946f743682416734f4f2917d85d4151f
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2015-05-27 23:26:47 +0530

    added new media q file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
 var highlightLink = function(){
     console.log('inside');
-var path = window.location.pathname;
+	var path = window.location.pathname;
 
-$('.header .align-middle p').removeClass('active-url');
+	$('.header .align-middle p').removeClass('active-url');
 
-    if (path.indexOf('index.html') > 0)
-        $('[data-url=about]').addClass('active-url');
-    else if(path.indexOf('home.html') > 0)
-        $('[data-url=home]').addClass('active-url');
-    else if(path.indexOf('post-advertisement.html') > 0)
-        $('[data-url=adds]').addClass('active-url');
-    else if(path.indexOf('recommended.html') > 0)
-        $('[data-url=recommended]').addClass('active-url');
+	    if (path.indexOf('index.html') > 0)
+	        $('[data-url=about]').addClass('active-url');
+	    else if(path.indexOf('home.html') > 0)
+	        $('[data-url=home]').addClass('active-url');
+	    else if(path.indexOf('post-advertisement.html') > 0)
+	        $('[data-url=adds]').addClass('active-url');
+	    else if(path.indexOf('recommended.html') > 0)
+	        $('[data-url=recommended]').addClass('active-url');
 };
 
 $(document).ready(function(){
